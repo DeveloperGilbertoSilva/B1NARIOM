508fa76fba788a09f7f58009bc54f323e335784e
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2017-05-04 22:43:48 -0300

    Adicionando script para menu fadeIn

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#btnOpenMenu").click(function(){
+        $("#menuTec-sm").fadeIn(400);
+    });
+    $("#btnCloseMenu").click(function(){
+       $("#menuTec-sm").fadeOut(200); 
+    });
+});
