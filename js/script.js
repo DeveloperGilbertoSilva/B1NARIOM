e8ebbd120c8b0de144ddfd925075548774a01b8f
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2017-05-05 23:58:27 -0300

    Adicionando menu dropDown em tela_tecnico e ajustes geral

--- js/script.js
+++ js/script.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
     $("#btnCloseMenu").click(function(){
        $("#menuTec-sm").fadeOut(200); 
     });
+    
+    $("#liEquipamento-sm").click(function(){
+        $("#subMenuEquipamento-sm, #upMenu-sm").slideDown(300);
+    });
+    $("#upMenu-sm").click(function(){
+        $("#subMenuEquipamento-sm, #upMenu-sm").slideUp();
+    });
+    
+    $("#liEquipamento-lg").click(function(){
+        $("#subMenuEquipamento-lg, #upMenu-lg").slideDown(300);
+    });
+    $("#upMenu-lg").click(function(){
+        $("#subMenuEquipamento-lg, #upMenu-lg").slideUp();
+    });
+   
 });
